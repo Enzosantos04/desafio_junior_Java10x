def8b030d2b84674a2f592e72da1481ee27a1b69
enzosdev <enzohfs@gmail.com>
2025-12-28 13:42:47 -0300

    chore: Initial project setup

---
+++ src/test/java/enzosdev/desafio_junior/DesafioJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package enzosdev.desafio_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
