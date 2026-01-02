84ff8f6bae8cdb4b0b57bc81b9c9c52714f2cbe9
enzosdev <enzohfs@gmail.com>
2026-01-02 00:27:19 -0300

    feat: Product Entity with ManyToOne relation

--- src/main/java/enzosdev/desafio_junior/entity/Product.java
+++ src/main/java/enzosdev/desafio_junior/entity/Product.java
@@ -1,4 +1,30 @@
 package enzosdev.desafio_junior.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String name;
+
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+
+
 }
