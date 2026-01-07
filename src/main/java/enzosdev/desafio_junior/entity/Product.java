74fb2e7b244061f284240df978efb6711c714415
enzosdev <enzohfs@gmail.com>
2026-01-07 20:42:54 -0300

    feat: add missing getters, setters and constructors

--- src/main/java/enzosdev/desafio_junior/entity/Product.java
+++ src/main/java/enzosdev/desafio_junior/entity/Product.java
@@ -24,7 +24,46 @@ public class Product {
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
+    public Product(Long id, String name, BigDecimal price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 
+    public Product(){
 
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
