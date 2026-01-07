e065602132f9ed0775f5feaf754a3bcd72ace191
enzosdev <enzohfs@gmail.com>
2026-01-07 20:01:12 -0300

    feat: CreateCategory endpoint

--- src/main/java/enzosdev/desafio_junior/controller/CategoryController.java
+++ src/main/java/enzosdev/desafio_junior/controller/CategoryController.java
@@ -1,4 +1,32 @@
 package enzosdev.desafio_junior.controller;
 
+import enzosdev.desafio_junior.entity.Category;
+import enzosdev.desafio_junior.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("desafio/")
 public class CategoryController {
+
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+
+
+    @PostMapping("/categories")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        Category newCategory = categoryService.createCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCategory);
+    }
 }
