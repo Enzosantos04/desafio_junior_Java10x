2fc369dcbc92ae25e631d04bbe5cac42513712e1
enzosdev <enzohfs@gmail.com>
2026-01-07 19:29:29 -0300

    feat: service for category entity (BRs)

--- src/main/java/enzosdev/desafio_junior/service/CategoryService.java
+++ src/main/java/enzosdev/desafio_junior/service/CategoryService.java
@@ -1,4 +1,19 @@
 package enzosdev.desafio_junior.service;
 
+import enzosdev.desafio_junior.entity.Category;
+import enzosdev.desafio_junior.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category createCategory(Category category){
+       return categoryRepository.save(category);
+    }
 }
