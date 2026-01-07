0e5520bcd3224c088f002b07ae9cd6955521f21f
enzosdev <enzohfs@gmail.com>
2026-01-07 20:46:20 -0300

    feat: add business rule to create product within category

--- src/main/java/enzosdev/desafio_junior/service/ProductService.java
+++ src/main/java/enzosdev/desafio_junior/service/ProductService.java
@@ -1,4 +1,30 @@
 package enzosdev.desafio_junior.service;
 
+
+import enzosdev.desafio_junior.entity.Category;
+import enzosdev.desafio_junior.entity.Product;
+import enzosdev.desafio_junior.repository.CategoryRepository;
+import enzosdev.desafio_junior.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Product createProduct(Long categoryId, Product product){
+        Category category = categoryRepository.findById(categoryId).orElseThrow(()-> new RuntimeException("Category Not Found"));
+
+        product.setCategory(category);
+
+        return productRepository.save(product);
+
+    }
 }
