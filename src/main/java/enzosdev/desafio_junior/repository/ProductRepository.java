22d7d5eaaf1973ac5fcc6dd395c183de551f546e
enzosdev <enzohfs@gmail.com>
2026-01-07 19:28:48 -0300

    feat: Repository for Product entity using spring data jpa

--- src/main/java/enzosdev/desafio_junior/repository/ProductRepository.java
+++ src/main/java/enzosdev/desafio_junior/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package enzosdev.desafio_junior.repository;
 
-public interface ProductRepository {
+import enzosdev.desafio_junior.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
