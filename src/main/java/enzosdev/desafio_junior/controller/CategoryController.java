4c3384dfa2bfabbe967aa7783be667bf030b17ac
enzosdev <enzohfs@gmail.com>
2026-01-07 20:21:11 -0300

    feat: ListCategories endpoint

--- src/main/java/enzosdev/desafio_junior/controller/CategoryController.java
+++ src/main/java/enzosdev/desafio_junior/controller/CategoryController.java
@@ -5,10 +5,9 @@ import enzosdev.desafio_junior.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -23,10 +22,16 @@ public class CategoryController {
     }
 
 
-
     @PostMapping("/categories")
-    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
         Category newCategory = categoryService.createCategory(category);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCategory);
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>>CategoriesList(){
+        List<Category> categories = categoryService.listCategories();
+        return ResponseEntity.ok(categories);
+
     }
 }
