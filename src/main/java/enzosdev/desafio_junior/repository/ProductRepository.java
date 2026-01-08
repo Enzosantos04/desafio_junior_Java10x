42a93edc9f108a222a86f40565944dfb60aa4aac
enzosdev <enzohfs@gmail.com>
2026-01-08 00:25:14 -0300

    feat: derived query to find product by category id

--- src/main/java/enzosdev/desafio_junior/repository/ProductRepository.java
+++ src/main/java/enzosdev/desafio_junior/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package enzosdev.desafio_junior.repository;
 import enzosdev.desafio_junior.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategoryId(Long categoryId);
 }
