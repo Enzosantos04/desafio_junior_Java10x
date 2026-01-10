cbdfd8f4c10dfee68809afbc063b50b184c746e2
enzosdev <enzohfs@gmail.com>
2026-01-10 19:19:25 -0300

    feat: add data into the product table using CommandLineRunner

--- src/main/java/enzosdev/desafio_junior/config/DataInitializer.java
+++ src/main/java/enzosdev/desafio_junior/config/DataInitializer.java
@@ -1,4 +1,26 @@
 package enzosdev.desafio_junior.config;
 
-public class DataInitializer {
+import enzosdev.desafio_junior.entity.Category;
+import enzosdev.desafio_junior.entity.Product;
+import enzosdev.desafio_junior.repository.CategoryRepository;
+import enzosdev.desafio_junior.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+
+import java.math.BigDecimal;
+
+public class DataInitializer implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+
+    public DataInitializer(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        productRepository.save(new Product(null, "Soft Drink", new BigDecimal("10"), new Category(null, "Beverages")));
+    }
 }
