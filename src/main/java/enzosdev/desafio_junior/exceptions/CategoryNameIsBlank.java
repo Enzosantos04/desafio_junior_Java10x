7e96387e2638600ee2dc06c4572455ed05694bcf
enzosdev <enzohfs@gmail.com>
2026-01-11 23:01:11 -0300

    feat: add CategoryNameIsBlank exception

--- src/main/java/enzosdev/desafio_junior/exceptions/CategoryNameIsBlank.java
+++ src/main/java/enzosdev/desafio_junior/exceptions/CategoryNameIsBlank.java
@@ -1,7 +1,7 @@
 package enzosdev.desafio_junior.exceptions;
 
 public class CategoryNameIsBlank extends RuntimeException {
-  public CategoryNameIsBlank(String message) {
-    super(message);
-  }
+    public CategoryNameIsBlank(String message) {
+        super(message);
+    }
 }
