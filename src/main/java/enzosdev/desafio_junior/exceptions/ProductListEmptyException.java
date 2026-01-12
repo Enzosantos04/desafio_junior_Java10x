d646a8c6dab624fa1263c4cfda7b77a5988cda23
enzosdev <enzohfs@gmail.com>
2026-01-12 13:20:58 -0300

    feat: add exception for empty list

--- src/main/java/enzosdev/desafio_junior/exceptions/ProductListEmptyException.java
+++ src/main/java/enzosdev/desafio_junior/exceptions/ProductListEmptyException.java
@@ -1,7 +1,7 @@
 package enzosdev.desafio_junior.exceptions;
 
 public class ProductListEmptyException extends RuntimeException {
-  public ProductListEmptyException(String message) {
-    super(message);
-  }
+    public ProductListEmptyException(String message) {
+        super(message);
+    }
 }
